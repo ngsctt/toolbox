b14f2184ba0bc9210276e5935ba56062565a9bcb
Nathan Scott <dev@ngscott.net>
2025-07-13 03:25:35 +1000

    Improve caching

--- cache-inputs.js
+++ cache-inputs.js
@@ -2,7 +2,10 @@
 export function cacheInputs (cacheKey, inputElements) {
   const values = {};
   for (const element of inputElements) {
-    if (element?.value) values[element.id] = element.value;
+    if (!element) continue;
+    else if (element instanceof HTMLImageElement) values[element.id] = element.src;
+    else if (element?.type === 'checkbox') values[element.id] = element.checked;
+    else if (element?.value != undefined) values[element.id] = element.value;
   }
   localStorage.setItem(cacheKey, JSON.stringify(values));
 }
@@ -11,7 +14,10 @@ export function restoreInputs (cacheKey) {
   const values = JSON.parse(localStorage.getItem(cacheKey));
   if (!values) return;
   for (const [id, value] of Object.entries(values)) {
-    const elem = document.getElementById(id);
-    if (elem) elem.value = value;
+    const element = document.getElementById(id);
+    if (!element) continue;
+    else if (element instanceof HTMLImageElement) element.src = value;
+    else if (element.type === 'checkbox') element.checked = value;
+    else element.value = value;
   }
 }
